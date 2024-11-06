2e07ae813cbefde217a6173ece8c961ba494ca8f
ToukaSama <blacksummoner6659@gmail.com>
2024-11-06 14:20:35 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -9,13 +9,13 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Dᴇᴠ : <a href='https://t.me/LeviAckerman1709'>Lᴇᴠɪ</a>\n○ ᴍʏ ᴜᴘᴅᴀᴛᴇs : <a href='https://t.me/Ahjin_Sprt'>AʜJɪɴGᴜɪʟᴅ</a>\n○ Aɴɪᴍᴇ Cʜᴀɴɴᴇʟ : <a href='https://t.me/Dub_Crunchyroll_Hindi'>Aɴɪᴍᴇ Cʜᴀɴɴᴇʟ</a>\n○ Hᴇɴᴛᴀɪ : <a href='https://t.me/HanimeCafe'>Hᴇɴᴛᴀɪ</a>\n○ ᴀɴɪᴍᴇ ᴄʜᴀᴛ : <a href='https://t.me/Surveycorpschat'>ᴀɴɪᴍᴇ ᴄʜᴀᴛ</a></b>",
+            text = f"<b>○ Dᴇᴠ : <a href='https://t.me/About_meowtaro'>Touka Sama</a>\n○ ᴍʏ ᴜᴘᴅᴀᴛᴇs : <a href='https://t.me/Anime_Sovereign'>Aɳιɱҽ_ʂσʋҽɾҽιɠɳ</a>\n○ Aɴɪᴍᴇ Cʜᴀɴɴᴇʟ : <a href='https://t.me/Anime_Sovereign'>Aɳιɱҽ_ʂσʋҽɾҽιɠɳ</a>\n○ Ongoing : <a href='https://t.me/Ongoing_Sovereign'>Hᴇɴᴛᴀɪ</a>\n○ ᴀɴɪᴍᴇ ᴄʜᴀᴛ : <a href='https://t.me/About_meowtaro'>ᴀɴɪᴍᴇ ᴄʜᴀᴛ</a></b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
                     InlineKeyboardButton("ᴄʟᴏsᴇ", callback_data = "close"),
-                    InlineKeyboardButton('Aɴɪᴍᴇ Cʜᴀɴɴᴇʟ', url='https://t.me/Dub_Crunchyroll_Hindi')
+                    InlineKeyboardButton('Aɴɪᴍᴇ Cʜᴀɴɴᴇʟ', url='https://t.me/Anime_Sovereign')
                     ]
                 ]
             )
